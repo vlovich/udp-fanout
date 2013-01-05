732e69149838b9035d2f7d3dc562fea3224fbb67
Vitali Lovich <vlovich@gmail.com>
2013-01-04 19:21:00 -0800

    cleanup + get code to run when built with gcc

--- main.cpp
+++ main.cpp
@@ -1,153 +1,100 @@
-#include <unordered_set>
 #include <thread>
-#include <iostream>
-#include <vector>
-#include <mutex>
 
 #include "net.h"
+#include "locked_ostream.h"
+#include "client_set.h"
 
-using Client = std::tuple<std::string, int>;
-using scoped_lock = std::lock_guard<std::mutex>;
-
-namespace std {
-    template <>
-    struct hash<Client> {
-        size_t operator()(const Client& client) const {
-            return hash<string>()(get<0>(client)) * 31 +
-                   hash<int>()(get<1>(client));
-        }
-    };
-}
-
-class LockedOStream {
-public:
-    LockedOStream(std::ostream& ostream, std::mutex& ostream_mutex) 
-        : ostream(ostream)
-        , ostream_lock(ostream_mutex)
-    {
-    }
-    
-    template <class T>
-    LockedOStream& operator<<(const T& data) {
-        ostream << data;
-        return *this;
-    }
-    
-private:
-    std::ostream& ostream;
-    scoped_lock ostream_lock;
-};
-
-class ClientSet {
-public:
-    ClientSet() {
-    }
-    
-    ~ClientSet() {
-    }
-    
-    bool addClient(const std::string& address, int port) {
-        return addClient(std::tie(address, port));
-    }
-    
-    bool addClient(const Client& client) {
-        scoped_lock lock(clientsMutex);
-        return clients.insert(client).second;
-    }
-    
-    bool removeClient(const std::string& address, int port) {
-        return removeClient(std::tie(address, port));
-    }
-    
-    bool removeClient(const Client& client) {
-        scoped_lock lock(clientsMutex);
-        return clients.erase(client) > 0;
-    }
-    
-    std::vector<Client> getClientList() const {
-        scoped_lock lock(clientsMutex);
-        std::vector<Client> result;
-        result.reserve(clients.size());
-        for (auto i = clients.begin(), ni = clients.end(); i != ni; i++) {
-            result.push_back(*i);
-        }
-        return result;
-    }
-
-private:
-    mutable std::mutex clientsMutex;
-    std::unordered_set<Client> clients;
-};
 
 static std::mutex console_lock;
+
+#define cout_locked() LockedOStream(std::cout, console_lock)
+#define cerr_locked() LockedOStream(std::cerr, console_lock)
 
 /**
  * Any packet received on this port registers the
  * sender as a target for outflows.
  */
 static void outflow_admin(int port, ClientSet *registrations) {
-    Udp listener;
-    listener.bind("0.0.0.0", port);
+    try {
+        Udp listener;
+        listener.bind("0.0.0.0", port);
 
-    LockedOStream(std::cout, console_lock) << "Waiting for clients to register on " << port << "\n";
+        cout_locked() << "Waiting for clients to register on " << port << "\n";
 
-    while (true) {
-        std::string client;
-        int port;
-        std::string data;
-        std::tie(client, port, data) = listener.recv();
-        if (data == "unregister") {
-            if (registrations->removeClient(client, port)) {
-                LockedOStream(std::cout, console_lock) << "Unregistered " << client << ":" << port << "\n";
+        while (true) {
+            std::string client;
+            int port;
+            std::string data;
+            std::tie(client, port, data) = listener.recv();
+            if (data == "unregister") {
+                if (registrations->removeClient(client, port)) {
+                    cout_locked() << "Unregistered " << client << ":" << port << "\n";
+                } else {
+                    cerr_locked() << "Client " << client << ":" << port << " not registered or already unregistered\n";
+                }
+            } else if (data == "register") {
+                if (registrations->addClient(client, port)) {
+                    cout_locked() << "Registered client " << client << ":" << port << ".  Payload (" << data << ")\n";
+                } else {
+                    cerr_locked() << "Client " << client << ":" << port << " already registered\n";
+                }
             } else {
-                LockedOStream(std::cerr, console_lock) << "Client " << client << ":" << port << " not registered or already unregistered\n";
+                cerr_locked() << "Unsupported command (" << data << ") from client " << client << ":" << port << "\n";
             }
-        } else if (data == "register") {
-            if (registrations->addClient(client, port)) {
-                LockedOStream(std::cout, console_lock) << "Registered client " << client << ":" << port << ".  Payload (" << data << ")\n";
-            } else {
-                LockedOStream(std::cerr, console_lock) << "Client " << client << ":" << port << " already registered\n";
-            }
-        } else {
-            LockedOStream(std::cerr, console_lock) << "Unsupported command (" << data << ") from client " << client << ":" << port << "\n";
         }
+    } catch (const std::exception& e) {
+        cerr_locked() << "Unhandled exception in admin processor: " << e.what() << std::endl;
     }
 }
 
 static void udp_mirror(int incoming_port, ClientSet *registrations) {
-    Udp listener;
-    Udp mirror;
+    try {
+        Udp listener;
+        Udp mirror;
 
-    listener.bind("0.0.0.0", incoming_port);
+        listener.bind("0.0.0.0", incoming_port);
 
-    LockedOStream(std::cout, console_lock) << "Listening for incoming UDP data on " << incoming_port << "\n";
+        cout_locked() << "Listening for incoming UDP data on " << incoming_port << "\n";
 
-    while (true) {
-        std::string sender;
-        int sender_port;
-        std::string data;
-        std::tie(sender, sender_port, data) = listener.recv();
+        while (true) {
+            std::string sender;
+            int sender_port;
+            std::string data;
+            std::tie(sender, sender_port, data) = listener.recv();
 
-        auto registered = registrations->getClientList();
+            auto registered = registrations->getClientList();
 
-        for (const auto& client : registered) {
-            // TODO: spoof src IP/PORT to be sender/sender_port
-            mirror.send(std::get<0>(client), std::get<1>(client), data);
+            for (const auto& client : registered) {
+                // TODO: spoof src IP/PORT to be sender/sender_port
+                mirror.send(std::get<0>(client), std::get<1>(client), data);
+            }
+
+            cout_locked() << "Re-broadcast datagram " << data.size() << " bytes to " << registered.size() << " clients\n";
         }
-
-        LockedOStream(std::cout, console_lock) << "Re-broadcast datagram " << data.size() << " bytes to " << registered.size() << " clients\n";
+    } catch (const std::exception& e) {
+        cerr_locked() << "Unhandled exception in receiver: " << e.what() << std::endl;
     }
 }
 
 int main(int argc, char **argv) {
-    int incoming_port = 514;
-    int outflow_admin_port = 10000;
-    ClientSet registrations;
-    
-    std::thread mirror_thread(udp_mirror, incoming_port, &registrations);
-    mirror_thread.detach();
+    try {
+        int incoming_port = 514;
+        int outflow_admin_port = 10000;
+        ClientSet registrations;
+        
+        //cout_locked() << "Starting mirror thread" << std::endl;
+        std::thread mirror_thread(udp_mirror, incoming_port, &registrations);
 
-    std::thread admin_thread(outflow_admin, outflow_admin_port, &registrations);
-    admin_thread.join();
+        //cout_locked() << "Detaching mirror thread" << std::endl;
+        mirror_thread.detach();
+
+        //cout_locked() << "Starting admin thread" << std::endl;
+        std::thread admin_thread(outflow_admin, outflow_admin_port, &registrations);
+
+        //cout_locked() << "Waiting for admin thread to finish" << std::endl;
+        admin_thread.join();
+    } catch (const std::exception& e) {
+        cerr_locked() << "Unhandled exception in main: " << e.what() << std::endl;
+    }
 }
 
